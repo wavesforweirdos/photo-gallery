a05532ede2d9e5518f3d56432cd133c745a26223
wavesforweirdos <wavesforweirdos@gmail.com>
2023-02-02 01:08:39 +0100

    vue app init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+
+<template>
+  <RouterView />
+</template>
+
+<style>
+  @import url('https://fonts.googleapis.com/css?family=Roboto');
+
+  html { font-size: 22px; }
+  body { font-family: 'Roboto', sans-serif; }
+
+  a {
+    color: #0094FF;
+    text-decoration: none;
+  }
+
+  a:hover { color: #0074C6; }
+</style>
+
