34c56408347ce75dadf72f63af51c982aafb9a29
wavesforweirdos <wavesforweirdos@gmail.com>
2023-02-06 12:45:24 +0100

    photo detail viewer component

---
+++ src/views/PhotoView.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <Gallery />
+        <Photo />
+    </div>
+</template>
+  
+
+<script>
+import Gallery from '../components/Gallery.vue';
+import Photo from '../components/Photo.vue';
+
+export default {
+    name: 'photoView',
+    components: {
+        Gallery,
+        Photo,
+    },
+};
+</script>
+
